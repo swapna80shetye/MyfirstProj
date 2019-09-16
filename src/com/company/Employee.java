639e5301e9df0587d206c3a9545438720f386cb1
Swapna <sw@frisq.se>
2019-09-16 15:51:45 +0200

    Initial commit

---
+++ src/com/company/Employee.java
@@ -0,0 +1,51 @@
+package com.company;
+
+
+
+public class Employee {
+    private int age;
+    private int baseSalary;
+
+    public Employee()
+    {
+        setBaseSalary(1000);
+        setAge(18);
+    }
+
+
+
+    public Employee(int baseSalary,int age) {
+        setAge(age);
+        setBaseSalary(baseSalary);
+    }
+
+    private void setAge(int age) throws IllegalArgumentException {
+        if (age<=0) {
+            throw new IllegalArgumentException("age should be more than 40!!");
+        }
+        this.age=age;
+    }
+
+    private int getAge()
+    {
+        return age;
+    }
+
+
+    private int getBaseSalary() {
+        return baseSalary;
+    }
+
+    private void setBaseSalary(int baseSalary) {
+        if (baseSalary<=0) {
+            throw new IllegalArgumentException("baseSalary cannot be 0 or less !!");
+        }
+        this.baseSalary = baseSalary;
+    }
+
+
+    public int calculateBenifits()
+    {
+        return baseSalary*age;
+    }
+}
---
+++ src/com/company/printStars.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class printStars {
+    public static void main(String args[])
+    {
+
+        //creating an object of type employee
+        Employee firstEmployee= new Employee(2000, 30);
+                System.out.println("first employee salary is:"+firstEmployee.calculateBenifits());
+        Employee secondEmp=new Employee();
+        System.out.println("the second employee salary is: "+secondEmp.calculateBenifits());
+                /*printing the stars in this program
+        for (int i = 0; i <4 ; i++) {
+            for (int j = 0; j <i ; j++) {
+                System.out.print("* ");
+                           }
+            System.out.println();
+        }*/
+    }
+
+}
