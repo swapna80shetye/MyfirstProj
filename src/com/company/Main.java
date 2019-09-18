f8d10370a9548aeb2990a6ba39a874eeaf76471d
Swapna <sw@frisq.se>
2019-09-18 16:23:24 +0200

    Edit .gitignore to filter iml file

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        Book book1=new Book("swapna's receipe","swapna ofcourse",1000);
+        book1.print();
+    }
+
+
+}
